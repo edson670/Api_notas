1355deaf9d01dd83e29cb152c3977c12c314cc9b
Edson <j.edson.da.silva.s@gmail.com>
2022-08-20 22:10:12 -0300

    manipulando arquivo

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,29 @@
+const fs = require("fs")
+const path = require("path")
+const uploadConfig = require("../configs/upload")
+
+class DiskStorage {
+    async savefile (file){
+        await fs.promises.rename(
+            path.resolve(uploadConfig.TMP_FOLDER, file),
+            path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+        )
+
+        return file
+    }
+
+    async deleFile(file){
+        const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+
+        try{
+            await fs.promises.start(filePath)
+        }catch{
+            return
+        }
+
+        await fs.promises.unlink(filePath)
+
+    }
+}
+
+module.exports = DiskStorage
